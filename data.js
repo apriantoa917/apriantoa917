278c6c73e3e8627403c3c4c53d76880dcfd6d13b
apriantoa917 <apriantoa917@gmail.com>
2023-01-01 16:20:34 +0700

    simplify project : remove include file nav + footer, make web to single page (remove blog, projects folder), clean data to js array object to easy maintain, updates social links

---
+++ data.js
@@ -0,0 +1,195 @@
+const data = [
+    // {
+    //     projectId: "coding-coffee",
+    //     title: "Coding Coffee : Cafe Menu Catalogue",
+    //     year: "2022",
+    //     overview: "...",
+    //     tech: "Kotlin, MVVM, Clean-Architecture, Dependency Injection",
+    //     image: [
+    //         "...",
+    //         "..."
+    //     ],
+    //     roles: "...",
+    //     sneakPeak: "...",
+    // },
+    {
+        projectId: "insul-in",
+        title: "Insul.in : Early Diabetes Detection Apps - Bangkit Academy 2022 Capstone Project",
+        year: "2022",
+        overview: `<p>Insul.in is Developed apps made by Bangkit Academy 2022 Team to give impact on Medical Issue. In Insul.in you can check initial screening of the potential risk of Diabetes with answering several question (combined with Machine Learning) to prevents worst condition based the result.
+    </p>`,
+        tech: "Kotlin, MVVM, Firebase, Google Maps API, Lottie Files, Retrofit, Google Console Project",
+        image: [
+            "https://user-images.githubusercontent.com/47800225/173065444-e2e1636f-5052-40b2-b204-d85d8bca50fd.png",
+            "https://user-images.githubusercontent.com/47800225/173065409-04795ac4-583c-437f-afb5-e9a0189a9d9a.png",
+        ],
+        roles: "Android Developer",
+        sneakPeak: `<p>See Repository at
+            <a href="https://github.com/Insulin-bangkit-2022"><i class="bi bi-github"></i>
+                Github</a>
+        </p>,`,
+    },
+    {
+        projectId: "picnicker-food-voucher",
+        title: "UI UX Design - Picnicker : Culinary Voucher Apps",
+        year: "2022",
+        overview: `<p>Picnicker is a prototype application for culinary vouchers that helps users to find various food promos and makes it easier for them to buy culinary vouchers online. This project is related to Dicoding Course
+        <b><a href="https://www.dicoding.com/academies/335">"Belajar Dasar UX Design"</a></b> as final submission
+    </p>`,
+        tech: "Figma, Prototyping, UX Research",
+        image: ["https://www.dropbox.com/s/nhpz5gdnnnhs5q8/Picnicker.png?raw=1"],
+        roles: "UI / UX Designer",
+        sneakPeak: `<p>Play prototype at
+        <a href="https://www.figma.com/proto/iPHFZztIOa1hT3MOTeBaFb/Dicoding-UX-Research?node-id=76%3A1068&viewport=241%2C48%2C0.23&scaling=scale-down&starting-point-node-id=76%3A1119"><i
+                class="bi bi-play-circle"></i>
+            Figma</a> | See the documentation at
+        <a href="https://aprianto.notion.site/Dicoding-UX-Design-Submission-d346172789f34ebc83c3415b7c909ba7"><i
+                class="bi bi-eye"></i>
+            Notion</a>
+    </p>`,
+    },
+    {
+        projectId: "dico-story",
+        title: "Android - Dico Story : Dicoding User Story",
+        year: "2022",
+        overview: `<p>Dico Story is an Dicoding's social media applications. You can create account then use the app for share your moments on application, you also can get the latest story (updates) from another user. This project is related to Dicoding Course
+        <b><a href="https://www.dicoding.com/academies/352">"Belajar Pengembangan Aplikasi Android
+                Intermediate"</a></b> as final submission</p>`,
+        tech: "Kotlin, Retrofit2 Library, Remote Mediator & Paging3 (Offline-Online Mechanism & Infinite Scroll data, like instagram), LottieFiles (Animation), Google Maps API",
+        image: ["https://www.dropbox.com/s/vosrq0gxt1atcwu/Dico%20Story.png?raw=1"],
+        roles: "Fullstack Engineer",
+        sneakPeak: `<p>See Repository at
+        <a href="https://github.com/apriantoa917/android-dicoding-BPAAI"><i class="bi bi-github"></i>
+            Github</a>
+    </p>`,
+    },
+    {
+        projectId: "my-github",
+        title: "Android - My Github : Github User Search Apps",
+        year: "2022",
+        overview: `<p>My Github is a github user search app developed using Github API to retrieve user search & user info details. In app you can like specific user locally & you can change theme appereance. This project is related to Dicoding Course <b><a href="https://www.dicoding.com/academies/14">"Belajar
+        Fundamental Aplikasi Android (BFAA)"</a></b> as final submission</p>`,
+        tech: "Kotlin, Retrofit2 Library, Room Database (liked user), Data Store (theme appereance)",
+        image: ["https://www.dropbox.com/s/g7ysfs8wp1cvngc/My%20Github.png?raw=1"],
+        roles: "Fullstack Engineer",
+        sneakPeak: `<p>See Repository at
+        <a href="https://github.com/apriantoa917/android-dicoding-BFAA"><i class="bi bi-github"></i>
+            Github</a>
+    </p>`,
+    },
+    {
+        projectId: "angket-aak",
+        title: "Website - Angket AAK : Academic Service (BAAK) assessment website",
+        year: "2021",
+        overview: `<p>Angket is a website for the BAAK Dinamika University assessment to assess the services provided to students in Universitas Dinamika. This project is related to my internship in pursuing my undergraduate B.Tech studies
+        </p>`,
+        tech: "Laravel 8, PHP, MySQL",
+        image: [
+            "https://www.dropbox.com/s/p0m1v3nx6cbckkr/Screenshot%202022-03-26%20061650.png?raw=1",
+            "https://www.dropbox.com/s/qkqxhk70w4n9i7f/Screenshot%202022-03-26%20061545.png?raw=1",
+            "https://www.dropbox.com/s/6s9add5xfma34s5/Screenshot%202022-03-26%20061725.png?raw=1",
+            "https://www.dropbox.com/s/7rdlnth274ba378/Screenshot%202022-03-26%20061358.png?raw=1",
+            "https://www.dropbox.com/s/4va8t02yt883xe8/Screenshot%202022-03-26%20061418.png?raw=1",
+        ],
+        roles: "Fullstack Engineer",
+        sneakPeak: `<p>See website at
+        <a href="https://angketaak.dinamika.ac.id/"><i class="bi bi-link-45deg"></i>
+            Website Angket AAK</a>
+    </p>`,
+    },
+    {
+        projectId: "mb-store-apps",
+        title: "Android - Mitra Bangunan Store : E-commerce building materials",
+        year: "2021",
+        overview: `<p>Mitra Building Store is an e-commerce development project for building materials using a webview display from an existing website and additional login and push notification features related to user accounts.
+        </p>`,
+        tech: "Webview Application, Firebase Cloud Messaging",
+        image: ["https://www.dropbox.com/s/1ykd223j5j4wajr/MB%20STORE.png?raw=1"],
+        roles: `<p>
+        <ul>
+            <li>Develop Login feature (handle login with native android and API)</li>
+            <li>Develop Webview apps from existing website (with customized layouts)</li>
+            <li>Develop Push Notification feature in application related with webview apps with FCM</li>
+        </ul>
+    </p>`,
+        sneakPeak: `<p>See Apps at
+        <a href="https://play.google.com/store/apps/details?id=com.mitrabangunan.mitrabangunanstore"><i
+                class="bi bi-shop"></i>
+            Playstore</a>
+    </p>`,
+    },
+    {
+        projectId: "sidedi",
+        title: "Android - Si Dedi : Sistem Informasi Deteksi dini Diabetes",
+        year: "2020",
+        overview: `<p>Si Dedi is an android application for early detection of diabetes using the Naive Bayes Classification (NBC) method based on several symptoms experienced by the user. This project is related to the decision support system course (SPK)
+        using a classification system</p>`,
+        tech: "Java, Retfrofit, Firebase Realtime Database (Article list)",
+        image: [
+            "https://www.dropbox.com/s/sq31nmtepcsyh13/banner_mobile.png?raw=1",
+        ],
+        roles: "Android Developer",
+        sneakPeak: `<p>See Repository at
+        <a href="https://github.com/apriantoa917/android-UAS_SPK-SiDeDi"><i class="bi bi-github"></i>
+            Github</a>
+    </p>`,
+    },
+    {
+        projectId: "itung-ip",
+        title: "Android - Itung IP: GPA Calculator",
+        year: "2020",
+        overview: `<p>Itung IP is a GPA Calculator that developed to help my college friends project their IP (Indeks Prestasi). Itung IP apps allow user to save their score then the apps will automatically generate their IP, and user data will synced across
+        user google account.</p>`,
+        tech: "Java, Firebase Auth (login with Google), Firebase Realtime Database (Store data), Firebase Cloud Messaging",
+        image: [
+            "https://www.dropbox.com/s/jiiuoy756h0y4w1/Artboard%20%E2%80%93%202.png?raw=1",
+        ],
+        roles: "Android Developer",
+        sneakPeak: `<p>See Repository at
+        <a href="https://github.com/apriantoa917/android-itung_IP"><i class="bi bi-github"></i>
+            Github</a>
+    </p>`,
+    },
+    {
+        projectId: "bookookoo",
+        title: "Android - BooKooKoo : Bookstore Preview Android Apps",
+        year: "2019",
+        overview: `<p>BooKooKoo is an application to display a list and details of a book as well as a list of books relevant to the book. This is my first android application and is a submission of the dicoding course <b><a href="https://www.dicoding.com/academies/51">"Belajar Membuat Aplikasi Android untuk
+        Pemula"</a></b>.</p>`,
+        tech: "Java, RecyclerView",
+        image: ["https://www.dropbox.com/s/f15vr8mp83wkci4/BooKooKoo.png?raw=1"],
+        roles: "Android Developer",
+        sneakPeak: `<p>See Repository at
+        <a href="https://github.com/apriantoa917/Android-BooKooKoo"><i class="bi bi-github"></i>
+            Github</a>
+    </p>`,
+    },
+    {
+        projectId: "katrol",
+        title: `<h5>UI UX Design - Katrol : <span style="font-weight: 900; text-decoration: underline;">KAT</span>eRing <span style="font-weight: 900;text-decoration: underline">O</span>n<span style="font-weight: 900;text-decoration: underline">L</span>ine
+        (Online Food Catering)</h5>`,
+        year: "2019",
+        overview: `<p>Katrol is a prototype of an online catering order application that helps you to order catering from various food catering services. The concept helps you busy workers or first-time users looking for online catering for your event</p>`,
+        tech: "Adobe XD, Prototyping",
+        image: ["https://www.dropbox.com/s/gnu91uhvdxwoxof/KATROL.png?raw=1"],
+        roles: "UI / UX Designer",
+        sneakPeak: `<p>Play prototype at
+        <a href="https://xd.adobe.com/view/61a9345f-8701-49fb-4d2a-cb9b8bf1099e-1798/screen/977db440-e72e-4815-aea8-8d9c2b14beb0/?fullscreen"><i
+                class="bi bi-play-circle"></i>
+            Adobe XD</a>
+    </p>`,
+    },
+    // {
+    //     projectId: "...",
+    //     title: "...",
+    //     year: "...",
+    //     overview: "...",
+    //     tech: "...",
+    //     image: [
+    //         "...",
+    //         "..."
+    //     ],
+    //     roles: "...",
+    //     sneakPeak: "...",
+    // },
+];
